ef1865cf9c6ef46c97760b0b8f459bd845a1910f
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-25 00:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age)
+	{
+			this.name = name;
+			this.age = age;
+	}
+	get name(){
+		return name;
+	}
+	set name(name){
+		this.name = name;
+	}
+	get age(){
+		return age;
+	}
+	set age(age)
+	{
+		this.age = age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	function study(){
+		console.log(`${this.name} is studying`)
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	function teach(){
+		console.log(`${this.name} is teaching`)
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
